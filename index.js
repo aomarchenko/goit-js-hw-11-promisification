ca6423cf9cb84f88f10580d5c9150c2be9811300
Олександр Марченко <80917411+aomarchenko@users.noreply.github.com>
2021-09-21 21:49:32 +0300

    2

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Задача 1----------------------------------------------------------------
+
 const delay = ms => {
   return new Promise(resolve => {
     setTimeout(
@@ -10,9 +12,31 @@ const delay = ms => {
   });
 };
 
-const logger = time => console.log(`Resolved after ${time}ms`);
+const logger1 = time => console.log(`Resolved after ${time}ms`);
 
 // Вызовы функции для проверки
-delay(2000).then(logger); // Resolved after 2000ms
-delay(1000).then(logger); // Resolved after 1000ms
-delay(1500).then(logger); // Resolved after 1500ms
+delay(2000).then(logger1); // Resolved after 2000ms
+delay(1000).then(logger1); // Resolved after 1000ms
+delay(1500).then(logger1); // Resolved after 1500ms
+
+// Задача 2---------------------------------------------------------------
+
+const users = [
+  { name: 'Mango', active: true },
+  { name: 'Poly', active: false },
+  { name: 'Ajax', active: true },
+  { name: 'Lux', active: false },
+];
+
+const toggleUserState = (allUsers, userName) => {
+  return Promise.resolve(
+    allUsers.map(user => (user.name === userName ? { ...user, active: !user.active } : user)),
+  );
+};
+
+const logger2 = updatedUsers => console.table(updatedUsers);
+
+toggleUserState(users, 'Mango').then(logger2);
+toggleUserState(users, 'Lux').then(logger2);
+
+// Задача 3---------------------------------------------------------------------
