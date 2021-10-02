1bddbd9d72eb49513f930178b72942cdb0100cd9
Олександр Марченко <80917411+aomarchenko@users.noreply.github.com>
2021-10-02 17:25:53 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,43 +1,43 @@
-// Задача 1----------------------------------------------------------------
+// // Задача 1----------------------------------------------------------------
 
-const delay = ms => {
-  return new Promise(resolve => {
-    setTimeout(
-      () => {
-        resolve(` ${ms}`);
-      },
+// const delay = ms => {
+//   return new Promise(resolve => {
+//     setTimeout(
+//       () => {
+//         resolve(` ${ms}`);
+//       },
 
-      ms,
-    );
-  });
-};
+//       ms,
+//     );
+//   });
+// };
 
-const logger1 = time => console.log(`Resolved after ${time}ms`);
+// const logger1 = time => console.log(`Resolved after ${time}ms`);
 
-// Вызовы функции для проверки
-delay(2000).then(logger1); // Resolved after 2000ms
-delay(1000).then(logger1); // Resolved after 1000ms
-delay(1500).then(logger1); // Resolved after 1500ms
+// // Вызовы функции для проверки
+// delay(2000).then(logger1); // Resolved after 2000ms
+// delay(1000).then(logger1); // Resolved after 1000ms
+// delay(1500).then(logger1); // Resolved after 1500ms
 
-// Задача 2---------------------------------------------------------------
+// // Задача 2---------------------------------------------------------------
 
-const users = [
-  { name: 'Mango', active: true },
-  { name: 'Poly', active: false },
-  { name: 'Ajax', active: true },
-  { name: 'Lux', active: false },
-];
+// const users = [
+//   { name: 'Mango', active: true },
+//   { name: 'Poly', active: false },
+//   { name: 'Ajax', active: true },
+//   { name: 'Lux', active: false },
+// ];
 
-const toggleUserState = (allUsers, userName) => {
-  return Promise.resolve(
-    allUsers.map(user => (user.name === userName ? { ...user, active: !user.active } : user)),
-  );
-};
+// const toggleUserState = (allUsers, userName) => {
+//   return Promise.resolve(
+//     allUsers.map(user => (user.name === userName ? { ...user, active: !user.active } : user)),
+//   );
+// };
 
-const logger2 = updatedUsers => console.table(updatedUsers);
+// const logger2 = updatedUsers => console.table(updatedUsers);
 
-toggleUserState(users, 'Mango').then(logger2);
-toggleUserState(users, 'Lux').then(logger2);
+// toggleUserState(users, 'Mango').then(logger2);
+// toggleUserState(users, 'Lux').then(logger2);
 
 // Задание 3--------------------------------------------------------
 
@@ -49,22 +49,24 @@ const makeTransaction = transaction => {
   const delay = randomIntegerFromInterval(200, 500);
   return new Promise((resolve, reject) => {
     const canProcess = Math.random() > 0.3;
+    const time = delay;
+    const id = transaction.id;
 
     setTimeout(() => {
       if (canProcess) {
-        resolve(transaction.id, delay);
+        resolve({ id, time });
       } else {
-        reject(transaction.id);
+        reject({ id, time });
       }
-    }, delay);
+    }, time);
   });
 };
 
-const logSuccess = (id, time) => {
+const logSuccess = ({ id, time }) => {
   console.log(`Transaction ${id} processed in ${time}ms`);
 };
 
-const logError = id => {
-  console.warn(`Error processing transaction ${id}. Please try again later.`);
+const logError = ({ id, time }) => {
+  console.warn(`Error processing transaction ${id}. Please try again after ${time} ms.`);
 };
 makeTransaction({ id: 71, amount: 230 }).then(logSuccess).catch(logError);
